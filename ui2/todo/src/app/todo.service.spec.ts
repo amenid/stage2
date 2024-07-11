f8d4c055a3bc767a4462b8cd2019b77e8a20fc1b
amenid <ameniaydiii@gmail.com>
2024-07-11 11:48:52 +0200

    aa

--- ui2/todo/src/app/todo.service.spec.ts
+++ ui2/todo/src/app/todo.service.spec.ts
@@ -1,20 +1,77 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing'; 
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 import { TodoService } from './todo.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms'; // Importer FormsModule
 
-describe('TodoService', () => {
-  let service: TodoService;
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let todoService: TodoService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], //  التصحيح  هنا
-    });
-    service = TestBed.inject(TodoService);
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, FormsModule], // Ajouter FormsModule
+      declarations: [AppComponent],
+      providers: [
+        {
+          provide: TodoService,
+          useValue: {
+            getTasks: () => of([]),
+            addTask: () => of({}),
+            deleteTask: () => of({}),
+            updateTask: () => of({})
+          }
+        }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    todoService = TestBed.inject(TodoService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get tasks from the service on init', () => {
+    const mockTasks = [
+      { id: 1, task: 'Task 1' },
+      { id: 2, task: 'Task 2' }
+    ];
+
+    spyOn(todoService, 'getTasks').and.returnValue(of(mockTasks)); 
+
+    component.ngOnInit(); 
+
+    expect(component.tasks).toEqual(mockTasks);
   });
 
+  it('should add a new task', () => {
+    spyOn(todoService, 'addTask').and.callThrough();
+    const newTask = 'New Task';
+
+    component.newtask = newTask; // Assurez-vous que le nom est correct
+    component.add_tasks(); // Assurez-vous que le nom est correct
+
+    expect(todoService.addTask).toHaveBeenCalledWith(newTask);
+  });
+
+  it('should delete a task', () => {
+    spyOn(todoService, 'deleteTask').and.callThrough();
+    const taskIdToDelete = 1;
+
+    component.delete_tasks(taskIdToDelete); // Assurez-vous que le nom est correct
+
+    expect(todoService.deleteTask).toHaveBeenCalledWith(taskIdToDelete);
+  });
+
+  it('should update a task', () => {
+    spyOn(todoService, 'updateTask').and.callThrough();
+    const taskIdToUpdate = 1;
+    const updatedTask = 'Updated Task';
+
+    component.updated_task_value = updatedTask; // Assurez-vous que le nom est correct
+    component.update_task(taskIdToUpdate); // Assurez-vous que le nom est correct
+
+    expect(todoService.updateTask).toHaveBeenCalledWith(taskIdToUpdate, updatedTask);
+  });
 });
--- ui3/todo/src/app/environment.ts
+++
@@ -1,8 +0,0 @@
-// src/app/environment.ts
-
-export const environment = {
-  production: false,
-  apiUrl: 'https://2e5a-197-0-108-96.ngrok-free.app'
-
-};
-
